85aa3616cec4bc740086d28c3c293e1f8d59251f
Archana Gandhi <74424757+Archana-gandhi@users.noreply.github.com>
2021-10-04 21:18:50 +0530

    Added header

---
+++ main.js
@@ -0,0 +1,13 @@
+// Menu Show
+function showMenu(toggleId, navId) {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+
+        });
+    }
+}
+showMenu('nav-toggle','nav-menu')
