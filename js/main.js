98ba322aeb0d07c5b8b2601ef3189ce068c2ad1f
Harshit Paliwal <harshitpaliwal95@gmail.com>
2021-10-18 21:53:43 +0530

    added topScroll

--- js/main.js
+++ js/main.js
@@ -1,44 +1,58 @@
 // Menu Show
 function showMenu(toggleId, navId) {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId);
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show');
-
-        });
-    }
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu("nav-toggle", "nav-menu");
 
-function nestedLinkHandler (link) {
-    const interviewBooks = document.getElementById("inteview-book-container")
-    const codingBooks = document.getElementById("coding-books-container");
-    if (link === "interview") {
-        codingBooks.style.display = "none";
-        interviewBooks.style.display = "block";
-    } else if (link === "codingLanguage"){
-        codingBooks.style.display = "block";
-        interviewBooks.style.display = "none";
-    } else {
-        codingBooks.style.display = "block";
-        interviewBooks.style.display = "block";
-    }
+function nestedLinkHandler(link) {
+  const interviewBooks = document.getElementById("inteview-book-container");
+  const codingBooks = document.getElementById("coding-books-container");
+  if (link === "interview") {
+    codingBooks.style.display = "none";
+    interviewBooks.style.display = "block";
+  } else if (link === "codingLanguage") {
+    codingBooks.style.display = "block";
+    interviewBooks.style.display = "none";
+  } else {
+    codingBooks.style.display = "block";
+    interviewBooks.style.display = "block";
+  }
 }
 
 function displayCategories(e) {
-    if (e.target.id !== "category-container") {
-        document.getElementById('category-nested-links').style.removeProperty("opacity");
-        document.getElementById('category-nested-links').style.removeProperty("visibility");
-        return;
-    }
+  if (e.target.id !== "category-container") {
+    document
+      .getElementById("category-nested-links")
+      .style.removeProperty("opacity");
+    document
+      .getElementById("category-nested-links")
+      .style.removeProperty("visibility");
+    return;
+  }
 
-    if(document.getElementById('category-nested-links').style.opacity) {
-        document.getElementById('category-nested-links').style.removeProperty("opacity");
-        document.getElementById('category-nested-links').style.removeProperty("visibility");
-    } else {
-        document.getElementById('category-nested-links').style.opacity = 1;
-        document.getElementById('category-nested-links').style.visibility = "visible";
-    }
+  if (document.getElementById("category-nested-links").style.opacity) {
+    document
+      .getElementById("category-nested-links")
+      .style.removeProperty("opacity");
+    document
+      .getElementById("category-nested-links")
+      .style.removeProperty("visibility");
+  } else {
+    document.getElementById("category-nested-links").style.opacity = 1;
+    document.getElementById("category-nested-links").style.visibility =
+      "visible";
+  }
+}
+
+// scroll to top function
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
